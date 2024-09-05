297f30803245a72194cfd13bb7ed5bd23efab5e2
Zeng Aoting <141926014+zengaoting@users.noreply.github.com>
2024-09-05 11:36:17 +0800

    Update run_gmixcon.sh

--- run_gmixcon.sh
+++ run_gmixcon.sh
@@ -3,5 +3,5 @@ for seed in 1314
 do 
     CUDA_VISIBLE_DEVICES=0  python -u ./src/g_mixcon.py --data_path . --model GIN --dataset IMDB-BINARY \
         --epoch 20 --lr 0.001 --saliency  False --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
-        --lambda_value 0.5 --saliency_ratio 0.8  --criterion  SupConLoss --warmup 0 --augsup False --num_layers 4 
+        --lambda_value 0.5 --saliency_ratio 0.8  --criterion  SupConMixLoss_G --warmup 0 --augsup False --num_layers 4 
 done
